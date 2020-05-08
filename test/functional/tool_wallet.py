d0c0587a5cbf776ace620e3e1a110b323d8a6f66
Overcooked Panda <overcookedpanda@gmail.com>
2020-05-07 20:19:46 -0500

    Initial fixes for QT daemon.

--- test/functional/tool_wallet.py
+++ test/functional/tool_wallet.py
@@ -6,7 +6,6 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test pexa-wallet."""
-"""Test vbitcoin-wallet."""
 
 import hashlib
 import os
