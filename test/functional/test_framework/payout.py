7064c70c617d57a136774d6616bd82191ebc6eb5
Overcooked Panda <overcookedpanda@gmail.com>
2020-06-08 09:52:56 -0500

    update POW_PAYOUT amount for functional tests

--- test/functional/test_framework/payout.py
+++ test/functional/test_framework/payout.py
@@ -3,4 +3,4 @@
 # https://www.veriblock.org
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-POW_PAYOUT = 50
+POW_PAYOUT = 45
