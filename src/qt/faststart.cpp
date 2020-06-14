cabdc6bd50a59ef164cbba5293695b95b8951590
Stackout <me@ryanhein.com>
2020-06-14 15:06:41 -0600

    wip

--- src/qt/faststart.cpp
+++ src/qt/faststart.cpp
@@ -299,7 +299,7 @@ void FastStart::DownloadSnapshotUrl()
 void FastStart::SnapshotUrlDownloaded(QNetworkReply* reply) 
 {
     if(reply->error()) {
-        ui->statusLabel->setText(ErrorText(tr(reply->errorString()))); 
+        ui->statusLabel->setText(ErrorText(tr(reply->errorString().toStdString()))); 
         QTimer::singleShot(ERROR_WAIT, this, SLOT(TryAgain()));
     } else {
         auto url_and_sha = QString::fromStdString(reply->readAll().toStdString()).trimmed().split(' ');
