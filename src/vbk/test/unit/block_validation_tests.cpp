19dada4ddc0527a7e4ae0a32a65c3f3b04090701
Andrew Lekar <andrey_lekar@adoriasoft.com>
2020-12-02 12:45:14 +0000

    Merkle tests

--- src/vbk/test/unit/block_validation_tests.cpp
+++ src/vbk/test/unit/block_validation_tests.cpp
@@ -1,15 +1,22 @@
-#include <boost/test/unit_test.hpp>
-#include <primitives/block.h>
-#include <streams.h>
-#include <version.h>
+// Copyright (c) 2019-2020 Xenios SEZC
+// https://www.veriblock.org
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include <boost/test/unit_test.hpp>
+#include <chainparams.h>
+#include <consensus/validation.h>
+#include <test/util/setup_common.h>
+#include <validation.h>
+#include <vbk/pop_service.hpp>
 #include <vbk/test/util/consts.hpp>
+#include <vbk/test/util/e2e_fixture.hpp>
 
 #include <string>
 
 inline std::vector<uint8_t> operator""_v(const char* s, size_t size)
 {
-    return std::vector<uint8_t>(s, s + size);
+    return std::vector<uint8_t>{s, s + size};
 }
 
 BOOST_AUTO_TEST_SUITE(block_validation_tests)
@@ -17,18 +24,19 @@ BOOST_AUTO_TEST_SUITE(block_validation_tests)
 static altintegration::PopData generateRandPopData()
 {
     altintegration::ValidationState state;
+
     // add PopData
     auto atvBytes = altintegration::ParseHex(VeriBlockTest::defaultAtvEncoded);
     auto streamATV = altintegration::ReadStream(atvBytes);
     altintegration::ATV atv;
     altintegration::DeserializeFromVbkEncoding(streamATV, atv, state);
-    BOOST_CHECK(state.IsValid());
+    assert(state.IsValid());
 
     auto vtbBytes = altintegration::ParseHex(VeriBlockTest::defaultVtbEncoded);
     auto streamVTB = altintegration::ReadStream(vtbBytes);
     altintegration::VTB vtb;
     altintegration::DeserializeFromVbkEncoding(streamVTB, vtb, state);
-    BOOST_CHECK(state.IsValid());
+    assert(state.IsValid());
 
     altintegration::PopData popData;
     popData.atvs = {atv};
@@ -62,6 +70,33 @@ BOOST_AUTO_TEST_CASE(block_serialization_test)
 
     BOOST_CHECK(decoded_block.GetHash() == block.GetHash());
     BOOST_CHECK(decoded_block.popData == block.popData);
+}
+
+BOOST_AUTO_TEST_CASE(block_network_passing_test)
+{
+    // Create random block
+    CBlock block;
+    block.hashMerkleRoot.SetNull();
+    block.hashPrevBlock.SetNull();
+    block.nBits = 10000;
+    block.nNonce = 10000;
+    block.nTime = 10000;
+    block.nVersion = 1 | VeriBlock::POP_BLOCK_VERSION_BIT;
+
+    altintegration::PopData popData = generateRandPopData();
+
+    block.popData = popData;
+
+    CDataStream stream(SER_NETWORK, PROTOCOL_VERSION);
+}
+
+BOOST_FIXTURE_TEST_CASE(BlockPoPVersion_test, E2eFixture)
+{
+    for (size_t i = 0; i < 400; ++i) {
+        CreateAndProcessBlock({}, cbKey);
+    }
+
+    auto block = CreateAndProcessBlock({}, cbKey);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
